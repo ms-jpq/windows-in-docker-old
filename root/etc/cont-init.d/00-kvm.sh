2bc315844fbaa23d432a7de0b3a94c00e55ae44c
Fly me to the moon <git@bigly.dog>
2020-06-14 02:09:43 -0700

    modprobe

--- root/etc/cont-init.d/00-kvm.sh
+++ root/etc/cont-init.d/00-kvm.sh
@@ -4,20 +4,19 @@ set -eu
 set -o pipefail
 
 
-mknod /dev/kvm c 10 232
-chown root:root /dev/kvm
-chmod g+rw /dev/kvm
-
-
-
+modlist=""
 if grep -qs "^flags.* vmx" /proc/cpuinfo; then
   modlist="kvm_intel"
 elif grep -qs "^flags.* svm" /proc/cpuinfo; then
   modlist="kvm_amd"
 fi
 
-
 if [ -n "$modlist" ]; then
   modprobe -b "$modlist" || true
 fi
+
+
+mknod /dev/kvm c 10 232
+chown root:root /dev/kvm
+chmod g+rw /dev/kvm
 

48ebd619a235d00da8c54d520d13e79a1a95d2c2
Fly me to the moon <git@bigly.dog>
2020-06-14 23:14:54 -0700

    find exclusion

--- root/etc/cont-init.d/00-nat.py
+++ root/etc/cont-init.d/00-nat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from ipaddress import ip_address, ip_network
+from ipaddress import IPv4Address, IPv4Network, ip_address, ip_network
 from os import environ
 from socket import AddressFamily
 from subprocess import run
@@ -8,6 +8,13 @@ from sys import stderr
 from typing import Any, Dict, List
 
 from psutil import net_if_addrs
+
+
+private_ranges: List[IPv4Network] = [
+    ip_network("10.0.0.0/8"),
+    ip_network("172.16.0.0/12"),
+    ip_network("192.168.0.0/16"),
+]
 
 
 def main() -> None:
@@ -23,15 +30,11 @@ def main() -> None:
     mask = address.netmask
     network = ip_network(f"{addr}/{mask}", False)
 
-    prefix = network.prefixlen
-    int_addr = int(ip_address(addr))
-    complement = int_addr ^ (1 << (32 - prefix))
+    exclusions = (exclusion
+                  for pr in private_ranges
+                  for exclusion in pr.address_exclude(network))
+    exclusion = next(exclusions)
 
-    new_mask = prefix - 1
-    new_ip = ip_address(complement)
-    new_network = ip_network(f"{new_ip}/{new_mask}", False)
-
-    print(new_network)
   else:
     print(f"ERROR! -- No IPv4 addr for {mac_lf}", file=stderr)
     exit(1)

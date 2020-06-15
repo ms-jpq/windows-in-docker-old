7951aab30dae6569b6464d5e4b6e2ce915249e77
Fly me to the moon <git@bigly.dog>
2020-06-15 09:53:45 -0700

    back to old kvm scrit

--- root/etc/cont-init.d/00-kvm.sh
+++ root/etc/cont-init.d/00-kvm.sh
@@ -1,23 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/with-contenv bash
 
 set -eu
 set -o pipefail
 
-
-modlist=""
-if grep -qs "^flags.* vmx" /proc/cpuinfo; then
-  modlist="kvm_intel"
-elif grep -qs "^flags.* svm" /proc/cpuinfo; then
-  modlist="kvm_amd"
-fi
-
-if [ -n "$modlist" ]; then
-  modprobe -b 'kvm' || true
-  modprobe -b "$modlist" || true
-fi
-
-
-mknod /dev/kvm c 10 232 || true
-chown root:kvm /dev/kvm || true
-chmod g+rw /dev/kvm || true
+/usr/share/qemu/init/qemu-kvm-init start
 

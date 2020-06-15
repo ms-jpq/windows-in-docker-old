ddc3a0ea4f1f5d946c9a644159343cad8f6a81ad
Fly me to the moon <git@bigly.dog>
2020-06-15 08:08:12 -0700

    :D

--- root/etc/cont-init.d/00-nat.py
+++ root/etc/cont-init.d/00-nat.py
@@ -1,13 +1,14 @@
 #!/usr/bin/with-contenv python3
 
 from ipaddress import IPv4Address, IPv4Network, ip_address, ip_network
+from json import loads
 from os import environ
 from os.path import join
 from shutil import get_terminal_size
 from socket import AddressFamily
 from subprocess import PIPE, run
 from sys import stderr, stdout
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Iterable, Iterator
 
 from psutil import net_if_addrs
 
@@ -16,18 +17,11 @@ _vmrc_ = "/vmrc"
 _nat_rc_ = "nat.xml"
 
 
-private_ranges: List[IPv4Network] = [
-    ip_network("10.0.0.0/8"),
-    ip_network("172.16.0.0/12"),
-    ip_network("192.168.0.0/16"),
-]
-
-
 def bold_print(message: str, sep="-", file=stdout) -> None:
   _, cols = get_terminal_size()
-  print(sep * cols, file=stderr)
-  print(message, file=stderr)
-  print(sep * cols, file=stderr)
+  print(sep * cols, file=file)
+  print(message, file=file)
+  print(sep * cols, file=file)
 
 
 def slurp(path: str) -> bytes:
@@ -41,10 +35,11 @@ def spit(path: str, data: bytes) -> None:
 
 
 def check_br() -> None:
-  ret = run(["show-bridges"], stdout=PIPE)
+  ret = run(["ip", "-j", "link", "show", "type", "bridge"], stdout=PIPE)
   if ret.returncode != 0:
     exit(ret.returncode)
-  bridges = ret.stdout.decode().split("\n")
+  json = loads(ret.stdout.decode())
+  bridges = (br["ifname"] for br in json)
   bridge = environ["VIRT_NAT_NAME"]
   if bridge in bridges:
     bold_print(f"ERROR! -- Already Exists :: {bridge}")
@@ -52,74 +47,73 @@ def check_br() -> None:
 
 
 def usable_ipv4(addr: Any) -> bool:
-  if addr.family != AddressFamily.AF_INET:
-    return False
-  ip = ip_address(addr.address)
-  if ip.is_loopback:
-    return False
-  return True
+  return (addr.family == AddressFamily.AF_INET
+          and not ip_address(addr.address).is_loopback)
 
 
-def p_addrs() -> List[Any]:
-  if_addrs = net_if_addrs()
-  names = (name
-           for name, addrs in if_addrs.items()
-           if any(usable_ipv4(addr) for addr in addrs))
-  mac_lf = environ.get("VIRT_NAT_IF") or next(names)
-  addrs = if_addrs[mac_lf]
-  return addrs
-
-
-def p_exclusions(network: IPv4Network) -> IPv4Network:
-  exclusions = (exclusion
-                for pr in private_ranges
-                for exclusion in pr.address_exclude(network)
-                if exclusion.num_addresses < 65535)
-  return exclusions
-
-
-def envsubst(address: Any) -> None:
+def p_network(address: Any) -> IPv4Network:
   addr = address.address
   mask = address.netmask
   network = ip_network(f"{addr}/{mask}", False)
+  return network
 
-  exclusions = p_exclusions(network)
-  new = next(exclusions)
-  it = new.hosts()
 
-  MASK = str(new.netmask)
-  ROUTER = str(next(it))
-  BEGIN = str(next(it))
-  END = str(new.broadcast_address - 1)
+def p_networks() -> Iterable[IPv4Network]:
+  if_addrs = net_if_addrs()
+  for addrs in if_addrs.values():
+    for addr in addrs:
+      if usable_ipv4(addr):
+        yield p_network(addr)
 
-  nat_rc = join(_vmrc_, _nat_rc_)
-  env = {**environ,
-         "MASK": MASK, "ROUTER": ROUTER,
-         "BEGIN": BEGIN, "END": END}
-  subst = "${VIRT_NAT_NAME},${ROUTER},${MASK},${BEGIN},${END}"
-  xml = slurp(nat_rc)
-  ret = run(["envsubst", subst], env=env, input=xml, stdout=PIPE)
 
+def p_non_overlapping_exclusions(networks: List[IPv4Network]) -> Iterable[IPv4Network]:
+  private_ranges: List[IPv4Network] = [
+      ip_network("10.0.0.0/8"),
+      ip_network("172.16.0.0/12"),
+      ip_network("192.168.0.0/16"),
+  ]
+  for private_range in private_ranges:
+    for network in networks:
+      for exclusion in private_range.address_exclude(network):
+        if exclusion.num_addresses < 65535:
+          yield exclusion
+
+
+def p_non_overlapping(networks: List[IPv4Network]) -> Iterable[IPv4Network]:
+  exclusions = p_non_overlapping_exclusions(networks)
+  for exclusion in exclusions:
+    if all(not exclusion.overlaps(network)
+           for network in networks):
+      yield exclusion
+
+
+def envsubst(values: Dict[str, str], path: str) -> None:
+  template = slurp(path)
+  subst = ",".join(f"${{key}}" for key in values.keys())
+  env = {**environ, **values}
+  ret = run(["envsubst", subst], env=env, input=template, stdout=PIPE)
   if ret.returncode != 0:
     exit(ret.returncode)
   else:
-    spit(nat_rc, ret.stdout)
+    spit(path, ret.stdout)
 
 
 def main() -> None:
   check_br()
-  addrs = p_addrs()
-  if not addrs:
-    raise Exception("Missing IPv4 if @ $VIRT_NAT_IF")
+  networks: List[IPv4Network] = [*p_networks()]
+  subnet: IPv4Network = next(p_non_overlapping(networks))
 
-  address = next(addr for addr in addrs
-                 if addr.family == AddressFamily.AF_INET)
-  if address:
-    envsubst(address)
+  it: Iterator[IPv4Network] = subnet.hosts()
+  MASK = str(subnet.netmask)
+  ROUTER = str(next(it))
+  BEGIN = str(next(it))
+  END = str(subnet.broadcast_address - 1)
 
-  else:
-    print(f"ERROR! -- No IPv4 addr for {mac_lf}", file=stderr)
-    exit(1)
+  nat_rc = join(_vmrc_, _nat_rc_)
+  env = {"VIRT_NAT_NAME": environ["VIRT_NAT_NAME"],
+         "MASK": MASK, "ROUTER": ROUTER,
+         "BEGIN": BEGIN, "END": END}
+  envsubst(env, nat_rc)
 
 
 main()

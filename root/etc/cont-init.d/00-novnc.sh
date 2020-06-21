e487548caa6457b7d4f00546429712bb8a8da50a
Fly me to the moon <git@bigly.dog>
2020-06-21 11:31:10 -0700

    update page title

--- root/etc/cont-init.d/00-novnc.sh
+++ root/etc/cont-init.d/00-novnc.sh
@@ -13,7 +13,9 @@ fi
 TEMP="$(envsubst '${PATH_PREFIX},${SOCKET_PREFIX}' < /etc/nginx/nginx.conf)"
 printf '%s' "$TEMP" > /etc/nginx/nginx.conf
 
+
 export PAGE_PREFIX="${SOCKET_PREFIX:1}/"
+export PAGE_TITLE="KVM - ${VM_NAME}"
 TEMP="$(envsubst '${PAGE_PREFIX},${PAGE_TITLE},${VNC_RESIZE},${RECON_DELAY}' < /novnc/index.html)"
 printf '%s' "$TEMP" > /novnc/index.html
 
